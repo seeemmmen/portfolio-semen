2b34e7309ff595a49c0bdd2f878e0cac0185d116
seeemmmen <seeemmmen@gmail.com>
2025-03-01 16:08:50 +0100

    adding animation and create respons on about page

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log('Happy developing ✨')
+
+
+
+
+let answer= ((a,b) => a + b );
+
+
+
+ setTimeout(()=>{
+        console.log(answer(20,50));
+    }, 5000);
+
+function scrollToSection(sectionId){
+    const sections = document.getElementsByClassName(sectionId);
+
+    if (sections.length > 0) {
+        sections[0].scrollIntoView({ behavior: 'smooth' });
+        console.log("Found section:", sections[0]);
+    } else {
+        console.error(`No element found with class '${className}'`);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector(`.header-text h1`).classList.add('animate');
+});
